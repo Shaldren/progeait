928320897ee8e990e1852564c4a3e0575ff95b8d
Shaldren <fouillen.dorian@orange.fr>
2018-10-10 16:09:06 +0200

    TESTDAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo
    OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- src/main/java/fr/eni/QCM/dal/dao/TestsDAO.java
+++ src/main/java/fr/eni/QCM/dal/dao/TestsDAO.java
@@ -3,4 +3,6 @@ package dal.dao;
 import bo.Tests;
 
 public interface TestsDAO extends GenericDAO<Tests,Integer> {
+
+    Tests selectByDescription(String description) throws DaoException;
 }
--- src/main/java/fr/eni/QCM/dal/dao/impl/TestsDaoImpl.java
+++ src/main/java/fr/eni/QCM/dal/dao/impl/TestsDaoImpl.java
@@ -4,31 +4,125 @@ import bo.Tests;
 import dal.dao.TestsDAO;
 import dal.exception.DaoException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestsDaoImpl implements TestsDAO {
+	
+	private static final String SELECT_ALL_TESTS_QUERY = "SELECT t.id as test_id, t.libelle as test_libelle, t.description as test_description, t.duree as test_duree, t.seuil_haut as test_seuil_haut, t.seuil_bas as test_seuil_bas FROM test t";
+    private static final String SELECT_ONE_TEST_QUERY = "SELECT t.id as test_id, t.libelle as test_libelle, t.description as test_description, t.duree as test_duree, t.seuil_haut as test_seuil_haut, t.seuil_bas as test_seuil_bas FROM test t where t.id = ?";
+    private static final String SELECT_ONE_TEST_BY_DESCRIPTION_QUERY = "SELECT t.id FROM test t where t.description = ?";
+    //private static final String INSERT_NOTE_QUERY = "INSERT INTO test(libelle, description, duree, seuil_haut, seuil_bas) VALUES (?, ?, ?, ?, ?)";
+    //private static final String DELETE_NOTE_QUERY = "DELETE FROM test WHERE id = ?";
+    //private static final String UPDATE_NOTE_QUERY = "UPDATE test SET libelle = ?, description = ?, duree = ?, seuil_haut = ?, seuil_bas = ? WHERE id = ?";
+    
+    private static TestsDaoImpl instance;
+    
+    private TestsDaoImpl() {
+        
+    }
+    
+    public static TestsDaoImpl getInstance() {
+        if(instance == null) {
+            instance = new TestsDaoImpl();
+        }
+        return instance;
+    }
+    
     @Override
     public Tests insert(Tests element) throws DaoException {
-        return null;
+    	//NOPE
     }
 
     @Override
-    public void update(Tests element) throws DaoException {
-
+    public void update(Tests element) throws DaoException {	
+    	//NOPE
     }
 
     @Override
     public void delete(Integer integer) throws DaoException {
-
+    	// NOPE
     }
 
     @Override
-    public Tests selectById(Integer integer) throws DaoException {
-        return null;
+    public Tests selectById(Integer id) throws DaoException {
+    	Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        Tests test = null;
+        
+        try {
+            connection = MSSQLConnectionFactory.get();
+            statement = connection.prepareStatement(SELECT_ONE_NOTE_QUERY);
+            
+            statement.setInt(1, id);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                note = resultSetToNote(resultSet);
+            }
+        } catch(SQLException e) {
+            throw new DaoException(e.getMessage(), e);
+        } finally {
+            ResourceUtil.safeClose(resultSet, statement, connection);
+        }
+        
+        return test;
     }
 
     @Override
     public List<Tests> selectAll() throws DaoException {
-        return null;
+    	Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        List<Tests> list = new ArrayList<>();
+        
+        try {
+            connection = MSSQLConnectionFactory.get();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(SELECT_ALL_NOTES_QUERY);
+
+            while (resultSet.next()) {
+                list.add(resultSetToNote(resultSet));
+            }
+        } catch(SQLException e) {
+            throw new DaoException(e.getMessage(), e);
+        } finally {
+            ResourceUtil.safeClose(resultSet, statement, connection);
+        }
+        
+        return list;
+    }
+    
+    @Override
+    public Tests selectByDescription(String description) throws DaoException {
+    	Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        Tests test = null;
+        
+        try {
+            connection = MSSQLConnectionFactory.get();
+            statement = connection.prepareStatement(SELECT_ONE_TEST_BY_DESCRIPTION_QUERY);
+            
+            statement.setString(1, description);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                note = resultSetToNote(resultSet);
+            }
+        } catch(SQLException e) {
+            throw new DaoException(e.getMessage(), e);
+        } finally {
+            ResourceUtil.safeClose(resultSet, statement, connection);
+        }
+        
+        return test;
     }
 }
